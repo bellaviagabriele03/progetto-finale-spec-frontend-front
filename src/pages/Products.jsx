2d8996a1744adf0c1f4a4ef65cd5e388388fd8fb
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2026-05-21 20:49:53 +0200

    FEAT: filtri per categoria DONE

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -10,11 +10,14 @@ export default function Products() {
 
     const { softwares } = useGlobalContext();
     const [query, setQuery] = useState("");
+    const [selectQuery, setSelectQuery] = useState("");
     console.log(softwares)
     const filterSoftware = useMemo(() => {
         if (!softwares) return []
-        return [...softwares].filter(p => p.title.toLowerCase().includes(query.toLowerCase()))
-    }, [softwares, query])
+        return [...softwares]
+            .filter(p => p.title.toLowerCase().includes(query.toLowerCase()))
+            .filter((p) => selectQuery !== "" ? p.category.toLowerCase() === selectQuery.toLocaleLowerCase() ? true : false : [...softwares])
+    }, [softwares, query, selectQuery])
 
     return (
         <>
@@ -30,11 +33,45 @@ export default function Products() {
                             <input
                                 value={query}
                                 onChange={e => setQuery(e.target.value)}
-                                className="bg-gradient-to-r from-emerald-600 to-gray-700 p-2 pl-9 rounded-3xl w-full"
+                                className="bg-gradient-to-r from-emerald-600 to-gray-700 p-4 pl-9 rounded-3xl w-full"
                                 type="text"
                                 placeholder="Cerca per titolo..." />
                         </div>
+                        <div>
+                            <select
+                                className="bg-gradient-to-r from-emerald-600 to-gray-700 p-4 rounded-3xl w-full mt-3"
+                                value={selectQuery}
+                                onChange={e => setSelectQuery(e.target.value)}
+                            >
+                                <option value="">Filtra per Categoria</option>
+                                <option value="videogiochi">Videogiochi</option>
+                                <option value="console">Console</option>
+                                <option value="accessories">Accessori</option>
+                            </select>
+                        </div>
+                        {selectQuery !== "" && (<>
+                            <div>
+                                <button
+                                    onClick={() => {
+                                        setSelectQuery("")
+                                        setQuery("")
+                                    }}
+                                    className="bg-gradient-to-r from-emerald-600 to-gray-700 p-4 rounded-3xl w-full mt-3 text-sky-400">Rimuovi filtri</button>
+                            </div>
+
+                        </>)}
+                        {query !== "" && (<>
+                            <div>
+                                <button
+                                    onClick={() => {
+                                        setSelectQuery("")
+                                        setQuery("")
+                                    }}
+                                    className="bg-gradient-to-r from-emerald-600 to-gray-700 p-4 rounded-3xl w-full mt-3 text-sky-400">Rimuovi filtri</button>
+                            </div>
+                        </>)}
                     </form>
+
                 </div>
                 <div className="" style={{ position: "relative", zIndex: 1 }}>
                     <div className="flex p-3 gap-2 flex-wrap flex-col">
