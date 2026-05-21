8f4b58425a2f9074ecda47c4783d975d1e875a63
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2026-05-21 21:31:18 +0200

    FEAT: filtro per ordine alfabetico per nome prodotto e categoria

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -5,19 +5,47 @@ import ParticleBackground from "../components/ParticleBackground";
 import { useGlobalContext } from "../context/GlobalContext";
 import { Search } from "lucide-react";
 
+const CATEGORY_ORDER = { "accessories": 0, "console": 1, "videogiochi": 2 }
 
 export default function Products() {
 
     const { softwares } = useGlobalContext();
     const [query, setQuery] = useState("");
     const [selectQuery, setSelectQuery] = useState("");
-    console.log(softwares)
+    const [show, setshow] = useState(false)
+    const [sortOrder, setSortOrder] = useState(1)
+    const [sortBy, setSortBy] = useState("videogiochi")
+
+    function handleSort(string) {
+        if (sortBy === string) {
+            setSortOrder(prev => prev * -1)
+        } else {
+            setSortBy(string)
+            setSortOrder(1);
+        }
+    }
+
+
+
     const filterSoftware = useMemo(() => {
         if (!softwares) return []
         return [...softwares]
             .filter(p => p.title.toLowerCase().includes(query.toLowerCase()))
             .filter((p) => selectQuery !== "" ? p.category.toLowerCase() === selectQuery.toLocaleLowerCase() ? true : false : [...softwares])
-    }, [softwares, query, selectQuery])
+            .sort((a, b) => {
+                if (sortBy === "title") {
+                    return a.title.localeCompare(b.title) * sortOrder;
+                }
+                if (sortBy === "category") {
+                    return (CATEGORY_ORDER[a.category] - CATEGORY_ORDER[b.category] * sortOrder);
+                }
+                return 0
+            })
+    }, [softwares, query, selectQuery, sortBy, sortOrder])
+
+
+
+
 
     return (
         <>
@@ -38,25 +66,52 @@ export default function Products() {
                                 placeholder="Cerca per titolo..." />
                         </div>
                         <div>
-                            <select
-                                className="bg-gradient-to-r from-emerald-600 to-gray-700 p-4 rounded-3xl w-full mt-3"
-                                value={selectQuery}
-                                onChange={e => setSelectQuery(e.target.value)}
-                            >
-                                <option value="">Filtra per Categoria</option>
-                                <option value="videogiochi">Videogiochi</option>
-                                <option value="console">Console</option>
-                                <option value="accessories">Accessori</option>
-                            </select>
+                            <button
+                                onClick={() => setshow(!show)}
+                                className="bg-gradient-to-r from-emerald-600 to-gray-700 p-4 rounded-3xl w-full mt-3 cursor-pointer text-sky-400">
+                                {show ? "NASCONDI" : "MOSTRA ALTRE OPZIONI"}
+                            </button>
                         </div>
+                        {show && (<>
+                            <div>
+                                <select
+                                    className="bg-gradient-to-r from-emerald-600 to-gray-700 p-4 rounded-3xl w-full mt-3"
+                                    value={selectQuery}
+                                    onChange={e => setSelectQuery(e.target.value)}
+                                >
+                                    <option value="">Filtra per Categoria</option>
+                                    <option value="videogiochi">Videogiochi</option>
+                                    <option value="console">Console</option>
+                                    <option value="accessories">Accessori</option>
+                                </select>
+                            </div>
+                            <div className="flex">
+                                <button
+                                    onClick={() => handleSort("title")}
+                                    className="bg-gradient-to-r from-emerald-500 to-gray-700 p-4 rounded-3xl w-full mt-3 cursor-pointer"
+                                >
+                                    Ordine alfabetico per nome prodotto {sortBy === "title" ? (sortOrder === 1 ? "▲" : "▼") : ""}
+                                </button>
+
+                                <button
+                                    onClick={() => handleSort("category")}
+                                    className="bg-gradient-to-r from-emerald-500 to-gray-700 p-4 rounded-3xl w-full mt-3 cursor-pointer"
+                                >
+                                    Ordine alfabetico per Categoria {sortBy === "category" ? (sortOrder === 1 ? "▲" : "▼") : ""}
+                                </button>
+                            </div>
+                        </>)}
+
+
                         {selectQuery !== "" && (<>
                             <div>
                                 <button
                                     onClick={() => {
                                         setSelectQuery("")
                                         setQuery("")
+                                        setshow(!show)
                                     }}
-                                    className="bg-gradient-to-r from-emerald-600 to-gray-700 p-4 rounded-3xl w-full mt-3 text-sky-400">Rimuovi filtri</button>
+                                    className="bg-gradient-to-r from-emerald-600 to-gray-700 p-4 rounded-3xl w-full mt-3 text-sky-400 cursor-pointer">Rimuovi filtri</button>
                             </div>
 
                         </>)}
@@ -66,8 +121,35 @@ export default function Products() {
                                     onClick={() => {
                                         setSelectQuery("")
                                         setQuery("")
+                                        setshow(!show)
                                     }}
-                                    className="bg-gradient-to-r from-emerald-600 to-gray-700 p-4 rounded-3xl w-full mt-3 text-sky-400">Rimuovi filtri</button>
+                                    className="bg-gradient-to-r from-emerald-600 to-gray-700 p-4 rounded-3xl w-full mt-3 text-sky-400 cursor-pointer">Rimuovi filtri</button>
+                            </div>
+                        </>)}
+                        {sortBy === "title" && (<>
+                            <div>
+                                <button
+                                    onClick={() => {
+                                        setSelectQuery("")
+                                        setQuery("")
+                                        setshow(!show)
+                                        setSortOrder(1)
+                                        setSortBy("videogiochi")
+                                    }}
+                                    className="bg-gradient-to-r from-emerald-600 to-gray-700 p-4 rounded-3xl w-full mt-3 text-sky-400 cursor-pointer">Rimuovi filtri</button>
+                            </div>
+                        </>)}
+                        {sortBy === "category" && (<>
+                            <div>
+                                <button
+                                    onClick={() => {
+                                        setSelectQuery("")
+                                        setQuery("")
+                                        setshow(!show)
+                                        setSortOrder(1)
+                                        setSortBy("videogiochi")
+                                    }}
+                                    className="bg-gradient-to-r from-emerald-600 to-gray-700 p-4 rounded-3xl w-full mt-3 text-sky-400 cursor-pointer">Rimuovi filtri</button>
                             </div>
                         </>)}
                     </form>
